29ace4553aca908635643fe8bbf3b3335c2a032a
zhengyuyangqwer <zhengyuyangchinasofti.com>
2015-10-31 12:31:46 +0800

    修改阅读统计人数显示不全的bug。
    修改阅读统计人数显示不全的bug。

--- LeaderDiandu/LeaderDiandu/ViewControllers/HBReadStatisticalStuNumCell.m
+++ LeaderDiandu/LeaderDiandu/ViewControllers/HBReadStatisticalStuNumCell.m
@@ -30,8 +30,9 @@
     
     //contentStuNumLabel
     self.contentStuNumLabel = [[UILabel alloc] init];
-    self.contentStuNumLabel.frame = CGRectMake(215, 0, 10, 50);
+    self.contentStuNumLabel.frame = CGRectMake(195, 0, 30, 50);
     self.contentStuNumLabel.text = @"0";
+    self.contentStuNumLabel.textAlignment = NSTextAlignmentRight;
     self.contentStuNumLabel.textColor = [UIColor colorWithHex:0x1E90FF];
     self.contentStuNumLabel.font = [UIFont boldSystemFontOfSize:20.0f];
     [self addSubview:self.contentStuNumLabel];
